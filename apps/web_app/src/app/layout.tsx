df314b0241c4cf295a54132769715df9c96e96a8
NehaPal2001 <neha.pal@sdettech.com>
2025-04-27 18:03:51 +0530

    home page complete

--- apps/web_app/src/app/layout.tsx
+++ apps/web_app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/NavBar"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NavBar />
+        <div className="mx-auto px-4 py-2.5">
+          {children}
+        </div>
       </body>
     </html>
   );
