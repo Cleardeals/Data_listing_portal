b8ca4ba4695961e770014ead8144c4bb968029ec
JJAIN02 <juheejain17@gmail.com>
2025-05-03 13:49:59 +0530

    new table changes

--- apps/web_app/src/app/page.tsx
+++ apps/web_app/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("/search");
+    router.push("/dashboard");
   }, [router]);
 
   return (
---
+++ apps/web_app/src/app/residential_rent_page/page.tsx
@@ -0,0 +1,13 @@
+import {PropertyTable} from "@/components/tables/PropertyTable"
+
+const Page = () => {
+  return (
+    <div>
+      <PropertyTable />
+    </div>
+  );
+};
+
+export default Page;
+
+
